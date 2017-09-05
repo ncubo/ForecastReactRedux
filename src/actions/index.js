e92257067ac714fd2aaf58e0998b4a17a952ed2c
Miguel <miguel@semanticrobot.com>
2017-09-04 19:26:29 -0700

    - cambios menores

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-
 const API_KEY = '58a5c117dff586e91d38d773c434bf20';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -4,7 +4,8 @@ export default function(state = [], action) {
     // console.log('Action received ', action);
     switch (action.type) {
       case FETCH_WEATHER:
-        // return state.contact([action.payload.data]); //use contact instead push
+        // return state.contact([action.payload.data]); //use contact instead push, concat create a new array (a copy)
+        // always return a new state( a copy), not the original state (I mean dont modify state)
         return [action.payload.data, ...state]; // both are same.. never use state.push
     }
     return state;
